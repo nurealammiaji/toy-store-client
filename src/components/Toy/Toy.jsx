eb8a1ae93cbc512f855e2ca173cb996d13eed276
nurealammiaji <nurealam.miaji@gmail.com>
2023-10-15 02:05:19 +0600

    toy delete function added

--- src/components/Toy/Toy.jsx
+++ src/components/Toy/Toy.jsx
@@ -1,9 +1,59 @@
 import { Link } from "react-router-dom";
 import { PiEyeBold, PiPenBold, PiTrashBold } from "react-icons/pi";
+import Swal from 'sweetalert2';
 
 const Toy = ({ toy }) => {
 
     const { _id, name, category, manufacturer, photoURL } = toy;
+
+    const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+            confirmButton: 'btn btn-success',
+            cancelButton: 'btn btn-danger'
+        },
+        buttonsStyling: false
+    })
+
+    const handleDeleteToy = (_id) => {
+        swalWithBootstrapButtons.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Delete',
+            cancelButtonText: 'Cancel',
+            reverseButtons: true
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/toys/${_id}`, {
+                    method: 'DELETE',
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data)
+                        if (data.acknowledged === true) {
+                            swalWithBootstrapButtons.fire(
+                                'Deleted!',
+                                'Toy has been deleted',
+                                'success'
+                            )
+                        }
+                    })
+                    .catch(error => {
+                        console.error(error)
+                    })
+            } else if (
+                /* Read more about handling dismissals below */
+                result.dismiss === Swal.DismissReason.cancel
+            ) {
+                swalWithBootstrapButtons.fire(
+                    'Cancelled',
+                    'Toy is safe now',
+                    'error'
+                )
+            }
+        })
+    }
 
     return (
         <div className="flex items-center justify-between gap-3 p-5 bg-base-200">
@@ -19,7 +69,7 @@ const Toy = ({ toy }) => {
                 <div className="join join-vertical">
                     <Link to={`/toys/view/${_id}`}><button className="w-full btn btn-info join-item"><PiEyeBold className="text-2xl" /> </button></Link>
                     <Link to={`/toys/edit/${_id}`}><button className="w-full btn btn-success join-item"><PiPenBold className="text-2xl" /></button></Link>
-                    <button className="btn btn-error join-item"><PiTrashBold className="text-2xl" /></button>
+                    <button onClick={() => handleDeleteToy(_id)} className="btn btn-error join-item"><PiTrashBold className="text-2xl" /></button>
                 </div>
             </div>
         </div>
